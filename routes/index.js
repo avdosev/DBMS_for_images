4e5494e75ebcf0c41826b620a75d6486099e3d5c
kyplyk <avdosev1@yandex.ru>
2019-05-24 18:11:47 +0400

    not working

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
-function initRoutes(app) {
+const bodyParser = require('body-parser');
 
+function initRoutes(app) {
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
+    app.get('/storage/:id', )
+    app.post('/storage')
 }
 
 
