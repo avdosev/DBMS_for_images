90d433e50fa6654e2b4f928ef66511b068433098
kyplyk <avdosev1@yandex.ru>
2019-05-26 18:52:17 +0400

    flex

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const drag_elem = document.body
     const elem = document.querySelector('.container')
+    const elem_inner = document.querySelector('.container-inner')
     
     drag_elem.addEventListener('drag', (event) => {
         console.log('puk')
@@ -12,17 +13,16 @@ document.addEventListener('DOMContentLoaded', () => {
         elem.classList.add('drag')
         //elem.classList.remove('drag');
     })
+    
+    function removeDrag() { elem.classList.remove('drag'); console.log('remove') }
+    drag_elem.addEventListener('dragleave', removeDrag)
 
-    drag_elem.addEventListener('dragend', (event) => {
-        console.log('end');
-        
-        elem.classList.remove('drag');
+    drag_elem.addEventListener('dragover', () => {
+        elem.classList.add('drag')
     })
 
-
-    
     drag_elem.addEventListener('drop', (event) => {
-        elem.classList.remove('drag');
+        removeDrag(elem)
         console.log(event.dataTransfer.files)
         console.log('drop')
     })
@@ -34,7 +34,16 @@ document.addEventListener('DOMContentLoaded', () => {
     drag_elem.addEventListener('copy', (event) => {
         console.log('copy')
     })
+
+    loadBtn.addEventListener('click', () => {
+        fileForm.click()
+    })
+
+    fileForm.addEventListener('change', () => {
+        // респонсе файл
+    })
 })
+
 
 function responseFile(file) {
     const data = new FormData();
