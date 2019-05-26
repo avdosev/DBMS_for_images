335ff916f6412b46143f3ec8dba5a8aa04631bee
kyplyk <avdosev1@yandex.ru>
2019-05-26 17:50:31 +0400

    help me

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const drag_elem = document.body
+    const elem = document.querySelector('.container')
     
+    drag_elem.addEventListener('drag', (event) => {
+        console.log('puk')
+        elem.classList.remove('drag');
+    })
+    
+    drag_elem.addEventListener('dragenter', (event) => {
+        console.log('enter')
+        elem.classList.add('drag')
+        //elem.classList.remove('drag');
+    })
+
+    drag_elem.addEventListener('dragend', (event) => {
+        console.log('end');
+        
+        elem.classList.remove('drag');
+    })
+
+
+    
+    drag_elem.addEventListener('drop', (event) => {
+        elem.classList.remove('drag');
+        console.log(event.dataTransfer.files)
+        console.log('drop')
+    })
+
+    drag_elem.addEventListener('load', (event) => {
+        console.log('load')
+    })
+
+    drag_elem.addEventListener('copy', (event) => {
+        console.log('copy')
+    })
 })
 
 function responseFile(file) {
